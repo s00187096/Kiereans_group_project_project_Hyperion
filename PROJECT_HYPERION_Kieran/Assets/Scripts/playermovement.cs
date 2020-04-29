1489c3425f377fba3806c15c10842b1bcdcf2234
s00187096 <55234394+s00187096@users.noreply.github.com>
2020-04-29 01:18:51 +0100

    rotation on planets

---
+++ PROJECT_HYPERION_Kieran/Assets/Scripts/playermovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    public float xySpeed = 18;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Mouse X");
+        float v = Input.GetAxis("Mouse Y");
+
+        LocalMove(h, v, xySpeed);
+    }
+    void LocalMove(float x, float y,float speed)
+    {
+        transform.localPosition += new Vector3(x, y, 0) * speed * Time.deltaTime;
+    }
+    void ClampPosition ()
+    {
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp01(pos.x);
+        pos.y = Mathf.Clamp01(pos.y);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+
+    void RotationLook(float h, float v,float speed)
+    {
+        aimTarget.localPosition = new Vector3(h, v, 1);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(aimTarget.position), Mathf.Deg2Rad * speed);
+    }
+    void HorizontalLean(Transform target, float axis, float leanLimit, float lerpTime)
+    {
+        Vector3 targetEulerAngels = target.localEulerAngles;
+        target.localEulerAngles = new Vector3(targetEulerAngels.x, targetEulerAngels.y, Mathf.LerpAngle(targetEulerAngels.z, -axis * leanLimit, lerpTime));
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = blue;
+    //    Gizmos.DrawWireSphere(aimTarget.posisition, .5f);
+    //    Gizmos.DrawSphere(aimTarget.posisiton, .15f);
+    //}
+}
