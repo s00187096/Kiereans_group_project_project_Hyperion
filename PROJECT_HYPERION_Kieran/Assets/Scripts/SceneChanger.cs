22936fef3013953d85b6763ee7a1da3b7e53f821
s00187096 <55234394+s00187096@users.noreply.github.com>
2020-05-09 21:04:49 +0100

    buttons implemented and working

--- PROJECT_HYPERION_Kieran/Assets/Scripts/SceneChanger.cs
+++ PROJECT_HYPERION_Kieran/Assets/Scripts/SceneChanger.cs
@@ -9,7 +9,7 @@ public class SceneChanger : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            SceneManager.LoadScene(1);
+            SceneManager.LoadScene(2);
         }
     }
 }
---
+++ PROJECT_HYPERION_Kieran/Assets/Scripts/quit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quit : MonoBehaviour
+{
+    public void exit()
+    {
+        Debug.Log("Has quit the game");
+        Application.Quit();
+    }
+}
