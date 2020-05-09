3e0f1d20ae20653007a8c014f64940af2f8ae5bd
s00187096 <55234394+s00187096@users.noreply.github.com>
2020-05-09 22:05:09 +0100

    redone buttons

---
+++ PROJECT_HYPERION_Kieran/Assets/Scripts/begin.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class begin : MonoBehaviour
+{
+    public void start() { SceneManager.LoadScene(1); }
+}
