9adb099a5f3b0df23b31e050b9e99aa03aefc91f
s00187096 <55234394+s00187096@users.noreply.github.com>
2020-05-10 14:00:53 +0100

    code keeps disappearing

---
+++ PROJECT_HYPERION_Kieran/Assets/Scripts/quit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void exit()
+    {
+        Debug.Log("player has quit");
+        Application.Quit();
+    }
+}
